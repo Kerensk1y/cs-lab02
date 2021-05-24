2845c335f0426b1976c24c920bbaaa6cf2b75786
Alice (RomanovKR) <keyslash123@gmail.com>
2021-05-24 13:59:53 +0300

    code: редактирование кода по формату

--- main.cpp
+++ main.cpp
@@ -7,9 +7,8 @@ int main()
     cout << "Enter A and B: ";
     double a, b;
     cin >> a >> b;
-
     cout << "A + B = " << a + b << '\n'
-    << "A - B = " << a - b << '\n';
-    cout << "A * B = " << a * b << '\n';
-    cout << "A / B = " << a / b << '\n';
+         << "A - B = " << a - b << '\n'
+         << "A * B = " << a * b << '\n'
+         << "A / B = " << a / b << '\n';
 }
